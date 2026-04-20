2d8321fa3a4e1cf29530e34728a2d5428851ab9a
ru1us <plagarufus@gmail.com>
2026-04-20 02:55:40 +0200

    r

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import App from "./App.tsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter basename="/LAYER_3">
+  <BrowserRouter>
     <App />
   </BrowserRouter>
 );
