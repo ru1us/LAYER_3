d1bbc8cbc8f643907fac436cab39322539acc469
ru1us <plagarufus@gmail.com>
2026-04-20 02:25:58 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "");
+
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <BrowserRouter>
+  <BrowserRouter basename={basename}>
     <App />
   </BrowserRouter>
 );
